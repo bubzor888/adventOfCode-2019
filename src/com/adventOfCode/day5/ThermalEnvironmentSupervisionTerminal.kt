9551f662549c7fec207c6e6b6e61b36cf80c7e7b
Mark Creamer <bubzor888@gmail.com>
2019-12-11 00:21:47 -0500

    day 5 part 1

--- src/com/adventOfCode/day5/ThermalEnvironmentSupervisionTerminal.kt
+++ src/com/adventOfCode/day5/ThermalEnvironmentSupervisionTerminal.kt
@@ -2,62 +2,212 @@ package com.adventOfCode.day5
 
 import java.io.File
 
+const val debug = true
+
 fun main(args: Array<String>) {
-    var fileName = "C:\\Users\\bubzo\\git\\AdventOfCode_2019\\src\\com\\adventOfCode\\day2\\input.txt"
+    var fileName = "C:\\Users\\bubzo\\git\\AdventOfCode_2019\\src\\com\\adventOfCode\\day5\\input.txt"
     var program = File(fileName).readText().split(",").map { it.toInt() }
 
-    println("Result: ${execute(program.toMutableList(), 12, 2)}")
+    /*
+    val test1Input = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9".split(",").map { it.toInt() }
+    println("Test1A: ${execute(test1Input.toMutableList(), 0) == 0}")
+    println("Test1B: ${execute(test1Input.toMutableList(), 1) == 1}")
 
-    //Now we're trying for 19690720
-    var found = false;
-    for (i in 1..100) {
-        for (j in 1..100) {
-            val result = execute(program.toMutableList(), i, j)
-            if (result == 19690720) {
-                println("Noun: $i, Verb: $j, 100 * i + j: ${100*i+j}")
-                found = true
-                break;
-            }
+    val test2Input = "3,3,1105,-1,9,1101,0,0,12,4,12,99,1".split(",").map { it.toInt() }
+    println("Test2A: ${execute(test1Input.toMutableList(), 0) == 0}")
+    println("Test2B: ${execute(test1Input.toMutableList(), 1) == 1}")
+*/
+    val test3Input = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99".split(",").map { it.toInt() }
+ //   for (i in 1..15) {
+    val i = 1
+        val testResult = execute(test3Input.toMutableList(), i)
+        if (i < 8) {
+            println("Test3.$i: Result $testResult : ${testResult == 999}")
+        } else if (i == 8) {
+            println("Test3.$i: Result $testResult : ${testResult == 1000}")
+        } else {
+            println("Test3.$i: Result $testResult : ${testResult == 1001}")
         }
-    }
+//    }
 
-    if (!found) {
-        println("Not found!")
-    }
+//    println("Part 1: ${execute(program.toMutableList(), 1)}")
+
+//    println("Part 2: ${execute(program.toMutableList(), 5)}")
 }
 
-fun execute(program: MutableList<Int>, input1: Int, input2: Int): Int {
+fun execute(program: MutableList<Int>, input: Int): Int {
     var pointer = 0
+    var numOfInstructions = 4
+    var result = -1
 
-    //Set inputs
-    program[1] = input1
-    program[2] = input2
-
-    var numOfinstructions = 4
+    if (debug) {
+        println("Initial State: $program")
+    }
 
     execute@ while (pointer < program.size) {
         val operation = program.elementAt(pointer)
+
+        //The opcode is the 2 right most digits
+        val opCode = operation % 100
+
+        var isJump = false
+
+        if (debug) {
+            println("Operation: $operation, opCode: $opCode")
+        }
 
         if (operation == 99) {
             //println("Success: $program[0]")
             break@execute
         }
 
-        val parameter1 = program.elementAt(pointer+1)
-        val parameter2 = program.elementAt(pointer+2)
+        val modeTypes = (operation / 100).toString().reversed()
+        if (debug) {
+            println("ModeInstructions: $modeTypes")
+        }
 
-        when (operation) {
-            1 -> program[program.elementAt(pointer+3)] = program.elementAt(parameter1) + program.elementAt(parameter2)
-            2 -> program[program.elementAt(pointer+3)] = program.elementAt(parameter1) * program.elementAt(parameter2)
+        when (opCode) {
+            1 -> {
+                val parameter1 = getParameter(modeTypes, 0, program, pointer+1)
+                val parameter2 = getParameter(modeTypes, 1, program, pointer+2)
+                //write parameters are always parameter mode
+                val parameter3 = program.elementAt(pointer+3)
+                program[parameter3] = parameter1 + parameter2
+                numOfInstructions = 4
+                if (debug) {
+                    println("Operation 1: Setting $parameter3 to $parameter1 + $parameter2")
+                }
+            }
+            2 -> {
+                val parameter1 = getParameter(modeTypes, 0, program, pointer+1)
+                val parameter2 = getParameter(modeTypes, 1, program, pointer+2)
+                //write parameters are always parameter mode
+                val parameter3 = program.elementAt(pointer+3)
+                program[parameter3] = parameter1 * parameter2
+                numOfInstructions = 4
+                if (debug) {
+                    println("Operation 2: Setting $parameter3 to $parameter1 * $parameter2")
+                }
+            }
+            3 -> {
+                val parameter = program[pointer+1]
+                program[parameter] = input
+                numOfInstructions = 2
+                if (debug) {
+                    println("Operation 3: Setting $parameter to $input")
+                }
+            }
+            4 -> {
+                result = getParameter(modeTypes,0, program, pointer+1)
+                numOfInstructions = 2
+                if (debug) {
+                    println("Test output from position ${pointer + 1}: $result")
+                    if (result != 0) {
+                        println("Failure, aborting")
+                        break@execute
+                    }
+                }
+            }
+            5 -> {
+                val parameter1 = getParameter(modeTypes,0, program, pointer+1)
+                if (parameter1 != 0) {
+                    val parameter2 = getParameter(modeTypes,0, program, pointer+2)
+                    pointer = parameter2
+                    isJump = true
+                    if (debug) {
+                        println("Operation 5: $parameter1 is non-zero. Successful jump to position $pointer")
+                    }
+                } else {
+                    numOfInstructions = 3
+                    if (debug) {
+                        println("Operation 5: $parameter1 is zero. No jump")
+                    }
+                }
+            }
+            6 -> {
+                val parameter1 = getParameter(modeTypes,0, program, pointer+1)
+                if (parameter1 == 0) {
+                    val parameter2 = getParameter(modeTypes,0, program, pointer+2)
+                    pointer = parameter2
+                    isJump = true
+                    if (debug) {
+                        println("Operation 6: $parameter1 is zero. Successful jump to position $pointer")
+                    }
+                } else {
+                    numOfInstructions = 3
+                    if (debug) {
+                        println("Operation 6: $parameter1 is non-zero. No jump")
+                    }
+                }
+            }
+            7 -> {
+                val parameter1 = getParameter(modeTypes, 0, program, pointer+1)
+                val parameter2 = getParameter(modeTypes, 1, program, pointer+2)
+                //write parameters are always parameter mode
+                val parameter3 = program.elementAt(pointer+3)
+                numOfInstructions = 4
+                if (parameter1 < parameter2) {
+                    program[parameter3] = 1
+                    if (debug) {
+                        println("Operation 7: $parameter1 < $parameter2: Setting position $parameter3 to 1")
+                    }
+                } else {
+                    program[parameter3] = 0
+                    if (debug) {
+                        println("Operation 7: $parameter1 >= $parameter2: Setting position $parameter3 to 0")
+                    }
+                }
+            }
+            8 -> {
+                val parameter1 = getParameter(modeTypes, 0, program, pointer+1)
+                val parameter2 = getParameter(modeTypes, 1, program, pointer+2)
+                //write parameters are always parameter mode
+                val parameter3 = program.elementAt(pointer+3)
+                numOfInstructions = 4
+                if (parameter1 == parameter2) {
+                    program[parameter3] = 1
+                    if (debug) {
+                        println("Operation 7: $parameter1 == $parameter2: Setting position $parameter3 to 1")
+                    }
+                } else {
+                    program[parameter3] = 0
+                    if (debug) {
+                        println("Operation 7: $parameter1 != $parameter2: Setting position $parameter3 to 0")
+                    }
+                }
+            }
             else -> {
                 println("Unknown operation found at $pointer. Terminating")
                 break@execute
             }
         }
 
-//        println("Program State: $program")
-        pointer+=numOfinstructions
+        if (debug) {
+            println("Program State: $program")
+            println("Pointer: $pointer -> ${pointer+numOfInstructions}")
+        }
+
+        if (!isJump) {
+            pointer += numOfInstructions
+        }
     }
 
-    return program[0]
+    return result
+}
+
+private fun getParameter(modeTypes: String, modeIndex: Int, program: List<Int>, pointer: Int): Int {
+    return when(modeTypes.elementAtOrElse(modeIndex) { '0' }.toString().toInt()) {
+        1 -> {
+            if (debug) {
+                println("getParameter for mode 1: pointer->$pointer : ${program[pointer]}")
+            }
+            program[pointer]
+        }
+        else -> {
+            if (debug) {
+                println("getParameter for mode 0: pointer->$pointer : program[pointer]->${program[pointer]} : ${program[program[pointer]]}")
+            }
+            program[program[pointer]]
+        }
+    }
 }
